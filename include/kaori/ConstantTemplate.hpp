56bb5676cdd4a208c6b68d0c281029d4e830b833
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-04-17 22:20:14 -0700

    Begin documenting all of the classes.

--- include/kaori/ConstantTemplate.hpp
+++ include/kaori/ConstantTemplate.hpp
@@ -5,13 +5,44 @@
 #include <deque>
 #include "utils.hpp"
 
+/**
+ * @file ConstantTemplate.hpp
+ *
+ * @brief Defines the `ConstantTemplate` class.
+ */
+
 namespace kaori {
 
+/**
+ * @brief Search a read sequence for a constant template.
+ *
+ * The template sequence is expected to contain constant regions interspersed with one or more variable regions.
+ * This class will find the location on the read sequence that matches the constant regions of the template (give or take any number of substitutions).
+ * Once a match is found, the variable regions can be extracted for look-up with, e.g., the `VariableLibrary` class.
+ *
+ * For efficiency, the search itself is done after converting all base sequences into a bit encoding.
+ * The maximum bit size of the encoding window is determined at compile-time using `N`.
+ * As four bits are used in the encoding, the maximum size of the constant template is `N / 4`.
+ *
+ * @tparam N Size of the bitset used to represent the constant template sequence.
+ */
 template<size_t N>
 class ConstantTemplate { 
 public:
+    /**
+     * Default constructor.
+     * This is only provided to enable composition, the resulting object should not be used until it is copy-assigned to a properly constructed instance.
+     */
     ConstantTemplate() {}
 
+    /**
+     * @param[in] s Pointer to a character array containing the constant template.
+     * Constant sequences should only contain `A`, `C`, `G` or `T` (or their lower-case equivalents).
+     * Variable regions should be marked with `-`.
+     * @param n Length of the array pointed to by `s`.
+     * @param f Should the search be performed on the forward strand of the read sequence?
+     * @param r Should the search be performed on the reverse strand of the read sequence?
+     */
     ConstantTemplate(const char* s, size_t n, bool f, bool r) : length(n), forward(f), reverse(r) {
         if (n * 4 > N) {
             throw std::runtime_error("maximum constant size should be " + std::to_string(N/4) + " nt");
@@ -55,10 +86,31 @@ public:
     }
 
 public:
+    /**
+     * @brief Details on the current match to the read sequence.
+     */
     struct MatchDetails {
+        /**
+         * Position of the match.
+         */
         size_t position = static_cast<size_t>(-1); // overflow should be sane.
+
+        /**
+         * Number of mismatches on the forward strand.
+         * If negative, no search was performed.
+         */
         int forward_mismatches = -1;
+
+        /**
+         * Number of mismatches on the reverse strand.
+         * If negative, no search was performed.
+         */
         int reverse_mismatches = -1;
+
+        /**
+         * Whether the match is at the end of the read sequence.
+         * If `true`, `next()` should not be called.
+         */
         bool finished = false;
 
         /**
@@ -73,6 +125,14 @@ public:
          */
     };
 
+    /**
+     * Begin a new search for the template in a read sequence.
+     *
+     * @param[in] seq Pointer to an array containing the read sequence.
+     * @param len Length of the read sequence.
+     *
+     * @return A `MatchDetails` object that can be passed to `next()`.
+     */
     MatchDetails initialize(const char* seq, size_t len) const {
         MatchDetails out;
         out.seq = seq;
@@ -102,6 +162,15 @@ public:
         return out;
     }
 
+    /**
+     * Find the next match in the read sequence.
+     * The first invocation will search for a match at position 0;
+     * this can be repeatedly called until `match.finished` is `true`.
+     *
+     * @param match A `MatchDetails` object produced by `initialize()`.
+     *
+     * @return `match` is updated with the details of the current match at a particular position on the read sequence.
+     */
     void next(MatchDetails& match) const {
         if (!match.bad.empty() && match.bad.front() == match.position) {
             match.bad.pop_front();
@@ -183,9 +252,19 @@ private:
         }
     }
 
-public:
+private:
     std::vector<std::pair<int, int> > forward_variables, reverse_variables;
 
+public:
+    /**
+     * Get the details about the variable regions in the constant template.
+     *
+     * @param reverse Should we return the coordinates of the variable regions when searching on the reverse strand?
+     *
+     * @return A vector of pairs where each pair specifies the start and one-past-the-end position of each variable region in the template.
+     * Coordinates are reported relative to the start of the template.
+     * If `reverse = true`, coordinates are reported after reverse-complementing the template sequence.
+     */
     const std::vector<std::pair<int, int> >& variable_regions(bool reverse = false) const {
         return (reverse ? reverse_variables : forward_variables);
     } 
