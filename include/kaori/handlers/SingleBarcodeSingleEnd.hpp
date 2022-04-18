0b46de6f0bf8b9e5de640a610eb79a5643dfb3d7
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2022-04-18 08:50:47 -0700

    Added handlers for common single-end barcode set-ups. (#3)

---
+++ include/kaori/handlers/SingleBarcodeSingleEnd.hpp
@@ -0,0 +1,69 @@
+#ifndef KAORI_SINGLE_BARCODE_SINGLE_END_HPP
+#define KAORI_SINGLE_BARCODE_SINGLE_END_HPP
+
+#include "../SimpleSingleMatch.hpp"
+#include <vector>
+
+namespace kaori {
+
+template<size_t N>
+class SingleBarcodeSingleEnd {
+public:
+    SingleBarcodeSingleEnd(const char* constant, size_t size, int strand, const std::vector<const char*>& variable, int mismatches = 0) : 
+        matcher(constant, size, strand != 1, strand != 0, variable, mismatches), counts(variable.size()) {}
+        
+    SingleBarcodeSingleEnd& set_first(bool t = true) {
+        use_first = t;
+        return *this;
+    }
+
+public:
+    struct State {
+        State() {}
+
+        State(typename SimpleSingleMatch<N>::SearchState s, size_t nvar) : search(std::move(s)), counts(nvar) {}
+
+        typename SimpleSingleMatch<N>::SearchState search;
+        std::vector<int> counts;
+    };
+
+    void process(State& state, const std::pair<const char*, const char*>& x) const {
+        bool found = false;
+        if (use_first) {
+            found = matcher.search_first(x.first, x.second - x.first, state.search);
+        } else {
+            found = matcher.search_best(x.first, x.second - x.first, state.search);
+        }
+        if (found) {
+            ++(state.counts[state.search.index]);
+        }
+    }
+
+    static constexpr bool use_names = false;
+
+public:
+    State initialize() const {
+        return State(matcher.initialize(), counts.size());
+    }
+
+    void reduce(State& s) {
+        matcher.reduce(s.search);
+        for (size_t i = 0; i < counts.size(); ++i) {
+            counts[i] += s.counts[i];
+        }
+    }
+
+private:
+    SimpleSingleMatch<N> matcher;
+    std::vector<int> counts;
+    bool use_first = true;
+
+public:
+    const std::vector<int>& results() const {
+        return counts;        
+    }
+};
+
+}
+
+#endif
