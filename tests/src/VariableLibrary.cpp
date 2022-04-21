ce5a3c2b2525a18078625a0abc65fa106200c164
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2022-04-20 21:08:38 -0700

    Support segmented mismatches in Trie, refine caching in VarLib. (#5)
    * Added trie subclass to handle segmented mismatches.
      This is intended for concatenated variable regions.
    * Re-enable specification of the number of mismatches in VarLib search.
      Only cache misses when this number is equal to the max mismatches.
      This enables a lower number of mismatches in searches.

--- tests/src/VariableLibrary.cpp
+++ tests/src/VariableLibrary.cpp
@@ -4,11 +4,11 @@
 #include <vector>
 #include "utils.h"
 
-TEST(VariableLibrary, Basic) {
+TEST(SimpleVariableLibrary, Basic) {
     std::vector<std::string> variables { "AAAA", "CCCC", "GGGG", "TTTT" };
     auto ptrs = to_pointers(variables);
 
-    kaori::VariableLibrary stuff(ptrs, 4);
+    kaori::SimpleVariableLibrary stuff(ptrs, 4);
     auto init = stuff.initialize();
 
     stuff.match("AAAA", init);
@@ -20,11 +20,11 @@ TEST(VariableLibrary, Basic) {
     EXPECT_EQ(init.index, -1);
 }
 
-TEST(VariableLibrary, ReverseComplement) {
+TEST(SimpleVariableLibrary, ReverseComplement) {
     std::vector<std::string> variables { "AAAA", "CCCC", "GGGG", "TTTT" };
     auto ptrs = to_pointers(variables);
 
-    kaori::VariableLibrary stuff(ptrs, 4, 0, true);
+    kaori::SimpleVariableLibrary stuff(ptrs, 4, 0, true);
     auto init = stuff.initialize();
 
     stuff.match("AAAA", init);
@@ -33,12 +33,12 @@ TEST(VariableLibrary, ReverseComplement) {
     EXPECT_EQ(init.index, 0);
 }
 
-TEST(VariableLibrary, Mismatches) {
+TEST(SimpleVariableLibrary, Mismatches) {
     std::vector<std::string> variables { "AAAA", "CCCC", "GGGG", "TTTT" };
     auto ptrs = to_pointers(variables);
 
     {
-        kaori::VariableLibrary stuff(ptrs, 4, 1);
+        kaori::SimpleVariableLibrary stuff(ptrs, 4, 1);
         auto init = stuff.initialize();
 
         stuff.match("AAAA", init);
@@ -57,7 +57,7 @@ TEST(VariableLibrary, Mismatches) {
     }
 
     {
-        kaori::VariableLibrary stuff(ptrs, 4, 2);
+        kaori::SimpleVariableLibrary stuff(ptrs, 4, 2);
         auto init = stuff.initialize();
 
         stuff.match("CCAC", init);
@@ -73,10 +73,10 @@ TEST(VariableLibrary, Mismatches) {
     }
 }
 
-TEST(VariableLibrary, Caching) {
+TEST(SimpleVariableLibrary, Caching) {
     std::vector<std::string> variables { "AAAA", "CCCC", "GGGG", "TTTT" };
     auto ptrs = to_pointers(variables);
-    kaori::VariableLibrary stuff(ptrs, 4, 1);
+    kaori::SimpleVariableLibrary stuff(ptrs, 4, 1);
 
     auto state = stuff.initialize();
 
@@ -84,6 +84,13 @@ TEST(VariableLibrary, Caching) {
     {
         stuff.match("AAAA", state);
         auto it = state.cache.find("AAAA");
+        EXPECT_TRUE(it == state.cache.end());
+    }
+
+    // No cache when the number of mismatches is lower than that in the constructor.
+    {
+        stuff.match("AATA", state, 0);
+        auto it = state.cache.find("AATA");
         EXPECT_TRUE(it == state.cache.end());
     }
 
@@ -105,6 +112,93 @@ TEST(VariableLibrary, Caching) {
     
     // Checking that the reduction works correctly.
     state.cache["AATA"].first = 2;
+    stuff.reduce(state);
+    EXPECT_TRUE(state.cache.empty());
+
+    {
+        stuff.match("AATA", state);
+        EXPECT_EQ(state.index, 2); // re-uses the cache value!
+    }
+}
+
+TEST(SegmentedVariableLibrary, Basic) {
+    std::vector<std::string> variables { "AAAAAA", "AACCCC", "AAGGGG", "AATTTT" };
+    auto ptrs = to_pointers(variables);
+
+    kaori::SegmentedVariableLibrary<2> stuff(ptrs, { 2, 4 }, { 0, 1 });
+    auto init = stuff.initialize();
+
+    stuff.match("AAAAAA", init);
+    EXPECT_EQ(init.index, 0);
+    stuff.match("AATTTT", init);
+    EXPECT_EQ(init.index, 3);
+
+    stuff.match("AACCAC", init); // 1 mismatch
+    EXPECT_EQ(init.index, 1);
+
+    stuff.match("AAccgg", init); // ambiguous.
+    EXPECT_EQ(init.index, -1);
+}
+
+TEST(SegmentedVariableLibrary, ReverseComplement) {
+    std::vector<std::string> variables { "AAAAAA", "AACCCC", "AAGGGG", "AATTTT" };
+    auto ptrs = to_pointers(variables);
+
+    kaori::SegmentedVariableLibrary<2> stuff(ptrs, { 2, 4 }, { 0, 1 }, true);
+    auto init = stuff.initialize();
+
+    stuff.match("AAAATT", init);
+    EXPECT_EQ(init.index, 3);
+    stuff.match("GGGGTT", init);
+    EXPECT_EQ(init.index, 1);
+
+    stuff.match("CCACTT", init);
+    EXPECT_EQ(init.index, 2);
+
+    stuff.match("GGCCTT", init); // ambiguous.
+    EXPECT_EQ(init.index, -1);
+}
+
+TEST(SegmentedVariableLibrary, Caching) {
+    std::vector<std::string> variables { "AAAA", "CCCC", "GGGG", "TTTT" };
+    auto ptrs = to_pointers(variables);
+    kaori::SegmentedVariableLibrary<2> stuff(ptrs, {2, 2}, {1, 1});
+
+    auto state = stuff.initialize();
+
+    // No cache when there is no mismatch.
+    {
+        stuff.match("AAAA", state);
+        auto it = state.cache.find("AAAA");
+        EXPECT_TRUE(it == state.cache.end());
+    }
+
+    // No cache when the number of mismatches is less than that in the constructor.
+    {
+        stuff.match("AATA", state, { 0, 0 });
+        auto it = state.cache.find("AATA");
+        EXPECT_TRUE(it == state.cache.end());
+    }
+
+
+    // Stored in cache for >1 mismatches.
+    {
+        stuff.match("AATA", state);
+        auto it = state.cache.find("AATA");
+        EXPECT_TRUE(it != state.cache.end());
+        EXPECT_EQ((it->second).index, 0);
+        EXPECT_EQ((it->second).total, 1);
+    }
+
+    {
+        stuff.match("ACTA", state);
+        auto it = state.cache.find("ACTA");
+        EXPECT_TRUE(it != state.cache.end());
+        EXPECT_EQ((it->second).index, -1);
+    }
+    
+    // Checking that the reduction works correctly.
+    state.cache["AATA"].index = 2;
     stuff.reduce(state);
     EXPECT_TRUE(state.cache.empty());
 
