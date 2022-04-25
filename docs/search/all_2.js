00ab3050e144d5d0cb66f5c81cd7366b441f3834
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-04-25 01:11:19 +0000

    Deploying to gh-pages from @ LTLA/kaori@888b5ea09e8fe9be70893bed1a9b59a1d2fc5e45 🚀

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['c_2b_2b_20library_20for_20counting_20screen_20data_0',['C++ library for counting screen data',['../index.html',1,'']]],
+  ['c_2b_2b_20library_20for_20counting_20barcodes_0',['C++ library for counting barcodes',['../index.html',1,'']]],
   ['combinatorialbarcodespairedend_1',['CombinatorialBarcodesPairedEnd',['../classkaori_1_1CombinatorialBarcodesPairedEnd.html#a6970f2be6e5245fa398122c0513bd650',1,'kaori::CombinatorialBarcodesPairedEnd::CombinatorialBarcodesPairedEnd()'],['../classkaori_1_1CombinatorialBarcodesPairedEnd.html',1,'kaori::CombinatorialBarcodesPairedEnd&lt; max_size &gt;']]],
   ['combinatorialbarcodespairedend_2ehpp_2',['CombinatorialBarcodesPairedEnd.hpp',['../CombinatorialBarcodesPairedEnd_8hpp.html',1,'']]],
   ['combinatorialbarcodessingleend_3',['CombinatorialBarcodesSingleEnd',['../classkaori_1_1CombinatorialBarcodesSingleEnd.html#a18958b442319e0888d127fd9d84abfe0',1,'kaori::CombinatorialBarcodesSingleEnd::CombinatorialBarcodesSingleEnd()'],['../classkaori_1_1CombinatorialBarcodesSingleEnd.html',1,'kaori::CombinatorialBarcodesSingleEnd&lt; max_size, num_variable &gt;']]],
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['c_2b_2b_20library_20for_20counting_20screen_20data_0',['C++ library for counting screen data',['../index.html',1,'']]]
+  ['c_2b_2b_20library_20for_20counting_20barcodes_0',['C++ library for counting barcodes',['../index.html',1,'']]]
 ];
