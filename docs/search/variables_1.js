a7c9d9fe91013403c48147477a579199f051b865
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-06-21 16:25:36 +0000

    Deploying to gh-pages from @ LTLA/kaori@5feff2448bd7799e4473e539ed9a9d2742ca4869 🚀

--- docs/search/variables_1.js
+++ docs/search/variables_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['index_0',['index',['../structkaori_1_1SimpleBarcodeSearch_1_1State.html#ac424ab64e2dd798903d4ef8ce5f04e5f',1,'kaori::SimpleBarcodeSearch::State::index()'],['../structkaori_1_1SegmentedBarcodeSearch_1_1State.html#acf848779e11f6741617e458c484c85fe',1,'kaori::SegmentedBarcodeSearch::State::index()'],['../structkaori_1_1SegmentedMismatches_1_1Result.html#a629831fff288a3221f6009da980ddee7',1,'kaori::SegmentedMismatches::Result::index()'],['../structkaori_1_1SimpleSingleMatch_1_1State.html#a2bc2b8e21011cc4b5b5bcf128c8fa851',1,'kaori::SimpleSingleMatch::State::index()']]]
+  ['index_0',['index',['../structkaori_1_1SimpleBarcodeSearch_1_1State.html#ac424ab64e2dd798903d4ef8ce5f04e5f',1,'kaori::SimpleBarcodeSearch::State::index'],['../structkaori_1_1SegmentedBarcodeSearch_1_1State.html#acf848779e11f6741617e458c484c85fe',1,'kaori::SegmentedBarcodeSearch::State::index'],['../structkaori_1_1SegmentedMismatches_1_1Result.html#a629831fff288a3221f6009da980ddee7',1,'kaori::SegmentedMismatches::Result::index'],['../structkaori_1_1SimpleSingleMatch_1_1State.html#a2bc2b8e21011cc4b5b5bcf128c8fa851',1,'kaori::SimpleSingleMatch::State::index']]]
 ];
--- docs/search/variables_3.js
+++ docs/search/variables_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['mismatches_0',['mismatches',['../structkaori_1_1SimpleBarcodeSearch_1_1State.html#aedd1d05b0ab22edbb8df1c792d17d5d7',1,'kaori::SimpleBarcodeSearch::State::mismatches()'],['../structkaori_1_1SegmentedBarcodeSearch_1_1State.html#af44eaa2a5d16f1587d85f2135ce947ea',1,'kaori::SegmentedBarcodeSearch::State::mismatches()'],['../structkaori_1_1SimpleSingleMatch_1_1State.html#abd5685abec77017d7075454126725cc8',1,'kaori::SimpleSingleMatch::State::mismatches()']]]
+  ['mismatches_0',['mismatches',['../structkaori_1_1SimpleBarcodeSearch_1_1State.html#aedd1d05b0ab22edbb8df1c792d17d5d7',1,'kaori::SimpleBarcodeSearch::State::mismatches'],['../structkaori_1_1SegmentedBarcodeSearch_1_1State.html#af44eaa2a5d16f1587d85f2135ce947ea',1,'kaori::SegmentedBarcodeSearch::State::mismatches'],['../structkaori_1_1SimpleSingleMatch_1_1State.html#abd5685abec77017d7075454126725cc8',1,'kaori::SimpleSingleMatch::State::mismatches']]]
 ];
