367a9e802292064ba385ebf5cb9993394097966e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-04-15 13:50:14 -0700

    Finished off the rest of the handlers.

--- include/kaori/handlers/RandomBarcodeSingleEnd.hpp
+++ include/kaori/handlers/RandomBarcodeSingleEnd.hpp
@@ -22,9 +22,9 @@ namespace kaori {
  * This handler will search the read for the barcoding element and count the frequency of each random barcode.
  * Random barcodes containing N's are allowed and will be counted separately.
  *
- * @tparam max_size Maximum length of the template sequences on both reads.
+ * @tparam max_size_ Maximum length of the template sequences on both reads.
  */
-template<size_t max_size>
+template<size_t max_size_>
 class RandomBarcodeSingleEnd {
 public:
     /**
@@ -53,28 +53,28 @@ public:
      * @param[in] template_seq Template sequence of the barcoding element.
      * This should contain exactly one variable region.
      * @param template_length Length of the template.
-     * This should be less than or equal to `max_size`.
+     * This should be less than or equal to `max_size_`.
      * @param options Optional parameters.
      */
     RandomBarcodeSingleEnd(const char* template_seq, size_t template_length, const Options& options) :
-        forward(search_forward(options.strand)),
-        reverse(search_reverse(options.strand)),
-        constant(template_seq, template_length, options.strand),
-        max_mm(options.max_mismatches),
-        use_first(options.use_first)
+        my_forward(search_forward(options.strand)),
+        my_reverse(search_reverse(options.strand)),
+        my_constant(template_seq, template_length, options.strand),
+        my_max_mm(options.max_mismatches),
+        my_use_first(options.use_first)
     {}
 
 private:
-    std::unordered_map<std::string, int> counts;
-    int total = 0;
+    std::unordered_map<std::string, int> my_counts;
+    int my_total = 0;
 
-    bool forward, reverse;
-    ScanTemplate<max_size> constant;
-    int max_mm;
-    bool use_first;
+    bool my_forward, my_reverse;
+    ScanTemplate<max_size_> my_constant;
+    int my_max_mm;
+    bool my_use_first;
 
     bool has_match(int obs_mismatches) const {
-        return (obs_mismatches >= 0 && obs_mismatches <= max_mm);
+        return (obs_mismatches >= 0 && obs_mismatches <= my_max_mm);
     }
 
 public:
@@ -92,7 +92,7 @@ public:
 
     void forward_match(const char* seq, size_t position, State& state) const {
         auto start = seq + position;
-        const auto& range = constant.forward_variable_regions()[0];
+        const auto& range = my_constant.forward_variable_regions()[0];
         std::copy(start + range.first, start + range.second, state.buffer.data());
 
         auto it = state.counts.find(state.buffer);
@@ -104,7 +104,7 @@ public:
     }
 
     void reverse_match(const char* seq, size_t position, State& state) const {
-        const auto& range = constant.forward_variable_regions()[0];
+        const auto& range = my_constant.forward_variable_regions()[0];
         auto start = seq + position + range.first;
         size_t len = state.buffer.size();
         for (size_t j = 0; j < len; ++j) {
@@ -121,31 +121,31 @@ public:
 
     void process(State& state, const std::pair<const char*, const char*>& x) const {
         auto read_seq = x.first;
-        auto deets = constant.initialize(read_seq, x.second - x.first);
+        auto deets = my_constant.initialize(read_seq, x.second - x.first);
 
-        if (use_first) {
+        if (my_use_first) {
             while (!deets.finished) {
-                constant.next(deets);
-                if (forward && has_match(deets.forward_mismatches)) {
+                my_constant.next(deets);
+                if (my_forward && has_match(deets.forward_mismatches)) {
                     forward_match(read_seq, deets.position, state);
                     break;
                 }
-                if (reverse && has_match(deets.reverse_mismatches)) {
+                if (my_reverse && has_match(deets.reverse_mismatches)) {
                     reverse_match(read_seq, deets.position, state);
                     break;
                 }
             }
 
         } else {
-            int best = max_mm + 1;
+            int best = my_max_mm + 1;
             bool best_forward = true;
             size_t best_position = 0;
             bool best_tied = false;
 
             while (!deets.finished) {
-                constant.next(deets);
+                my_constant.next(deets);
 
-                if (forward && has_match(deets.forward_mismatches)) {
+                if (my_forward && has_match(deets.forward_mismatches)) {
                     if (deets.forward_mismatches < best) {
                         best = deets.forward_mismatches;
                         best_position = deets.position;
@@ -156,7 +156,7 @@ public:
                     }
                 }
 
-                if (reverse && has_match(deets.reverse_mismatches)) {
+                if (my_reverse && has_match(deets.reverse_mismatches)) {
                     if (deets.reverse_mismatches < best) {
                         best = deets.reverse_mismatches;
                         best_position = deets.position;
@@ -168,7 +168,7 @@ public:
                 }
             }
 
-            if (!best_tied && best <= max_mm) { 
+            if (!best_tied && best <= my_max_mm) { 
                 if (best_forward) {
                     forward_match(read_seq, best_position, state);
                 } else {
@@ -190,20 +190,20 @@ public:
      * @cond
      */
     State initialize() const {
-        const auto& range = constant.forward_variable_regions()[0];
+        const auto& range = my_constant.forward_variable_regions()[0];
         return State(range.second - range.first);
     }
 
     void reduce(State& s) {
         for (const auto& pair : s.counts) {
-            auto it = counts.find(pair.first);
-            if (it != counts.end()) {
+            auto it = my_counts.find(pair.first);
+            if (it != my_counts.end()) {
                 it->second += pair.second;
             } else {
-                counts[pair.first] = pair.second;
+                my_counts[pair.first] = pair.second;
             }
         }
-        total += s.total;
+        my_total += s.total;
     }
     /**
      * @endcond
@@ -214,14 +214,14 @@ public:
      * @return Unordered map containing the frequency of each random barcode.
      */
     const std::unordered_map<std::string, int>& get_counts() const {
-        return counts;        
+        return my_counts;        
     }
 
     /**
      * @return Total number of reads processed by the handler.
      */
     int get_total() const {
-        return total;
+        return my_total;
     }
 };
 
