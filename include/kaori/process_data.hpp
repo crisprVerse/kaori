143f9c43c49d14a53c30f2546185f99ecff2ee37
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-05-30 19:30:47 +1000

    Consistently unlock mutex before notifying waiting threads.

--- include/kaori/process_data.hpp
+++ include/kaori/process_data.hpp
@@ -91,11 +91,12 @@ public:
             my_threads.emplace_back([run_job,this,&init_mut,&init_cv,&num_initialized](int thread) -> void { 
                 Helper env; // allocating this locally within each thread to reduce the risk of false sharing.
                 my_helpers[thread] = &env;
+
                 {
                     std::lock_guard lck(init_mut);
                     ++num_initialized;
-                    init_cv.notify_one();
                 }
+                init_cv.notify_one(); // only notify once the lock is released for better performance. 
 
                 while (1) {
                     std::unique_lock lck(env.mut);
@@ -116,6 +117,7 @@ public:
 
                     env.has_output = true;
                     env.available = true;
+                    lck.unlock(); // again, release the lock before notifying the waiting thread.
                     env.cv.notify_one();
                 }
             }, t);
